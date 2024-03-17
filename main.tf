cf437243b21ac3c8a357f8e2c66fff4ea1f3949a
Abiodun Lasisi <lasisiabioduntowobola@gmail.com>
2024-03-17 09:55:40 +0000

    updated security group name

--- main.tf
+++ main.tf
@@ -237,7 +237,7 @@ resource "aws_db_instance" "db-instance" {
   parameter_group_name = "default.mysql5.7"
   skip_final_snapshot  = true
   db_subnet_group_name = aws_db_subnet_group.default-db-sg.id
-  vpc_security_group_ids = [aws_security_group.db-sg]
+  vpc_security_group_ids = [aws_security_group.db-sg.id]
 }
 
 /**************************************************
